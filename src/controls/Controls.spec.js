9df669bd84a4591a0f8e2594a56a6d48d4febea0
davidhennig <davidjhennig@gmail.com>
2019-12-17 18:02:01 -0500

    finished

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -3,6 +3,13 @@
 import React from "react";
 import Controls from "./Controls";
 import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+test("Open/closed", () => {
+  const constProps = { closed: false, locked: false };
+  const { getByText } = render(<Controls {...constProps} />);
+  getByText(/close gate/i);
+});
 
 test("Button Test", () => {
   const constProps = { closed: false, locked: false };
@@ -16,4 +23,30 @@ test("Button Test", () => {
   fireEvent.click(button);
   rerender(<Controls {...constProps} />);
   expect(button.innerHTML).toBe("Open Gate");
+});
+
+test("Lock to Unlock", () => {
+  const constProps = { closed: true, locked: false };
+  const jestFn = jest.fn(() => {
+    constProps.locked = !constProps.locked;
+  });
+  const { getByText, rerender } = render(
+    <Controls {...constProps} toggleLocked={jestFn} />
+  );
+  const button = getByText(/lock gate/i);
+  fireEvent.click(button);
+  rerender(<Controls {...constProps} />);
+  expect(button.innerHTML).toBe("Unlock Gate");
+});
+
+test("Check Lock/Unlock", () => {
+  const constProps = { closed: false, locked: false };
+  const { getByText } = render(<Controls {...constProps} />);
+  expect(getByText(/lock gate/i).closest("button")).toHaveAttribute("disabled");
+});
+
+test("Check Open/Closed", () => {
+  const constProps = { closed: true, locked: true };
+  const { getByText } = render(<Controls {...constProps} />);
+  expect(getByText(/open gate/i).closest("button")).toHaveAttribute("disabled");
 });
